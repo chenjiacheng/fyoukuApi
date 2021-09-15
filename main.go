ebdb5ae89ee08deeb97aa2e1c93fb92953fc9689
Jason <405339556@qq.com>
2021-09-15 23:23:49 +0800

    登录注册功能

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	_ "fyoukuApi/routers"
+	"github.com/beego/beego/v2/client/orm"
 
 	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// 加载数据库
+	defaultdb, _ := beego.AppConfig.String("defaultdb")
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", defaultdb)
+
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
